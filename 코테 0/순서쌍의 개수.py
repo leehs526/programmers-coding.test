ae2ff94cff9c58c0ee2283f5de0f05c62e801aa2
이한설 <juicyiceyg@gmail.com>
2026-07-14 18:42:37 +0900

    순서쌍의 개수.py 만들기

---
+++ 코테 0/순서쌍의 개수.py
@@ -0,0 +1,10 @@
+두 숫자의 곱이 n인 자연수 순서쌍의 개수
+ex)n= 20 -> 곱이 20인 순서쌍은 (1, 20), (2, 10), (4, 5), (5, 4), (10, 2), (20, 1) 이므로 6을 return
+
+1)
+def solution(n):
+    answer = 0
+    for i in range(1,n+1):
+        if n%i==0:
+            answer += 1
+    return answer
