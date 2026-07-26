c60ee22413d3b33da03fea22743ba1b538d98b2c
이한설 <juicyiceyg@gmail.com>
2026-07-26 23:50:14 +0900

    Update 대문자.소문자.py

--- 코테 0/대문자.소문자.py
+++ 코테 0/대문자.소문자.py
@@ -3,3 +3,13 @@
 1)
 def solution(my_string):
     return my_string.swapcase()
+
+2)
+def solution(my_string):
+    answer = ''
+    for i in my_string:
+        if i.isupper():
+            answer += i.lower()
+        else:
+            answer += i.upper()
+    return answer
