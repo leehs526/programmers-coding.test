9583a5fbbe14870feceda4d35db3b9d06239581f
이한설 <juicyiceyg@gmail.com>
2026-07-29 23:11:32 +0900

    Update 약수(오답).py

--- 코테 0 O/약수(오답).py
+++ 코테 0 O/약수(오답).py
@@ -1,6 +1,12 @@
+1)
 def solution(n):
     answer = []
     for i in range(1, n+1): #for i in n 안됨!! n이 정수이기때문
         if n % i == 0:
             answer.append(i) # answer += i 안됨!! 리스트에 추가할때는 append 써야함
+    return answer
+
+2)
+def solution(n):
+    answer = [i for i in range(1, n+1) if n % i == 0]
     return answer
